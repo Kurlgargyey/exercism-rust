594572c3abbaf88790dc7826bb28f5417613e571
RK <nahsirkivar@gmail.com>
2024-01-17 10:49:45 +0100

    clearer merge function and no strings until larger workloads

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -3,16 +3,14 @@ use std::sync::mpsc;
 use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let mut root = HashMap::<char, usize>::new();
-
     if input.is_empty() {
-        return root;
+        return HashMap::new();
     }
 
-    const MIN_INPUT_SIZE: usize = 30;
+    const MIN_INPUT_SIZE: usize = 500;
 
     if input.len() <= MIN_INPUT_SIZE {
-        return string_frequencies(input.join(""));
+        return slice_frequencies(input);
     }
 
     let (output_tx, output_rx) = mpsc::channel();
@@ -39,10 +37,23 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
 
     drop(output_tx);
 
+    let mut root = HashMap::<char, usize>::new();
     while let Ok(branch) = output_rx.recv() {
         root = merge_frequency_maps(root, branch);
     }
     root
+}
+
+fn slice_frequencies(slice: &[&str]) -> HashMap<char, usize> {
+    let root = HashMap::<char, usize>::new();
+    slice.iter().fold(root, |map, line| {
+        line.chars()
+            .filter(|char| char.is_alphabetic())
+            .fold(map, |mut map, letter| {
+                *map.entry(letter.to_ascii_lowercase()).or_insert(0) += 1;
+                map
+            })
+    })
 }
 
 fn string_frequencies(string: String) -> HashMap<char, usize> {
@@ -60,8 +71,8 @@ fn merge_frequency_maps(
     root: HashMap<char, usize>,
     branch: HashMap<char, usize>,
 ) -> HashMap<char, usize> {
-    branch.into_iter().fold(root, |mut map, kv_pair| {
-        *map.entry(kv_pair.0).or_insert(0) += kv_pair.1;
+    branch.into_iter().fold(root, |mut map, (letter, count)| {
+        *map.entry(letter).or_insert(0) += count;
         map
     })
 }
