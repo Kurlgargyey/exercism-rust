22420a63b1551ec77918e0c37ccbabbffb5b51ea
RK <nahsirkivar@gmail.com>
2024-01-23 13:24:14 +0100

    guard against small input sizes

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -2,13 +2,11 @@ use rayon::prelude::*;
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], _worker_count: usize) -> HashMap<char, usize> {
-    input
-        .join("")
-        .par_chars()
-        .filter(|c| c.is_alphabetic())
-        .map(|c| c.to_ascii_lowercase())
-        .fold(HashMap::new, |map, letter| count_letter(map, letter, 1))
-        .reduce(HashMap::new, |root, branch| merge_counts(root, branch))
+    match input.len() {
+        0 => HashMap::new(),
+        x if x < 300 => slice_frequencies(input),
+        _ => par_frequencies(input),
+    }
 }
 
 fn count_letter(mut map: HashMap<char, usize>, letter: char, tally: usize) -> HashMap<char, usize> {
@@ -16,6 +14,25 @@ fn count_letter(mut map: HashMap<char, usize>, letter: char, tally: usize) -> Ha
         .and_modify(|count| *count += tally)
         .or_insert(tally);
     map
+}
+
+fn slice_frequencies(slice: &[&str]) -> HashMap<char, usize> {
+    let root = HashMap::<char, usize>::new();
+    slice.iter().fold(root, |map, line| {
+        line.chars()
+            .filter(|char| char.is_alphabetic())
+            .fold(map, |map, letter| count_letter(map, letter, 1))
+    })
+}
+
+fn par_frequencies(slice: &[&str]) -> HashMap<char, usize> {
+    slice
+        .join("")
+        .par_chars()
+        .filter(|c| c.is_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .fold(HashMap::new, |map, letter| count_letter(map, letter, 1))
+        .reduce(HashMap::new, |root, branch| merge_counts(root, branch))
 }
 
 fn merge_counts(root: HashMap<char, usize>, branch: HashMap<char, usize>) -> HashMap<char, usize> {
