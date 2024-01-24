7b57248aabee418c16d5552999744e13950e979a
RK <nahsirkivar@gmail.com>
2024-01-24 12:48:54 +0100

    correct import of hashmap struct

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,9 +1,10 @@
 #[macro_export]
-use std::collections::HashMap;
+
 macro_rules! hashmap {
     ($($key:expr => $value:expr),*) => {
         {
-            let mut temp_map = HashMap::new();
+            use ::std::collections::HashMap as hm;
+            let mut temp_map = hm::new();
             $(
                 temp_map.insert($key, $value);
             )*
