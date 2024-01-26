fe0b6be6dc4977341de4de95203957c9c217f039
RK <nahsirkivar@gmail.com>
2024-01-26 15:34:57 +0100

    almost done, just need to fix mistakenly compiling on a comma only argument

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,8 @@
 #[macro_export]
 
 macro_rules! hashmap {
-    ($($key:expr => $value:expr),*) => {
+    ($($key:expr => $value:expr),*$(,)?) => {
+        //                       ^ this captures a single trailing comma
         {
             use ::std::collections::HashMap as hm;
             let mut temp_map = hm::new();
