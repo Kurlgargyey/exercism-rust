89f60488ff0963deccac849661d28668998da0a7
RK <nahsirkivar@gmail.com>
2024-02-02 09:23:58 +0100

    explicitly handling trailing comma case

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,8 +1,18 @@
+#![recursion_limit = "256"]
 #[macro_export]
 
 macro_rules! hashmap {
-    ($($key:expr => $value:expr),*$(,)?) => {
-        //                       ^ this captures a single trailing comma
+    ($($key:expr => $value:expr,)*) => {
+        {
+            use ::std::collections::HashMap as hm;
+            let mut temp_map = hm::new();
+            $(
+                temp_map.insert($key, $value);
+            )*
+            temp_map
+        }
+    };
+    ($($key:expr => $value:expr),*) => {
         {
             use ::std::collections::HashMap as hm;
             let mut temp_map = hm::new();
