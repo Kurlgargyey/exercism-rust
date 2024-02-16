65d59dcbc94e3957e0039179884e2cd7987a7cf3
RK <nahsirkivar@gmail.com>
2024-02-16 15:14:55 +0100

    passing implementation

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+
 pub type Value = i32;
 pub type Result = std::result::Result<(), Error>;
 
 pub struct Forth {
     stack: Vec<Value>,
+    words: HashMap<String, String>,
+    token_stream: Vec<String>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -17,6 +21,8 @@ impl Forth {
     pub fn new() -> Forth {
         Forth {
             stack: Vec::<Value>::new(),
+            words: HashMap::<String, String>::new(),
+            token_stream: Vec::<String>::new(),
         }
     }
 
@@ -25,29 +31,45 @@ impl Forth {
     }
 
     pub fn eval(&mut self, input: &str) -> Result {
-        let token_stream = input
+        self.token_stream = input
             .split_whitespace()
-            .map(|token| token.to_ascii_uppercase());
-        for token in token_stream {
-            if let Ok(number) = token.parse::<i32>() {
-                self.stack.push(number)
-            } else {
-                let binary = "+ - * / SWAP OVER";
-                match token.as_str() {
-                    s if binary.split_whitespace().any(|operator| s == operator) => {
-                        self.binary_operation(&s)?
-                    }
-                    "DUP" => self.unary_operation(&token)?,
-                    "DROP" => self.unary_operation(&token)?,
-                    _ => return Err(Error::UnknownWord),
-                }
-            }
+            .map(|token| token.to_ascii_uppercase())
+            .rev()
+            .collect();
+
+        while let Some(token) = self.token_stream.pop() {
+            self.eval_token(&token)?;
         }
 
         Ok(())
     }
 
-    fn pop(&mut self) -> std::result::Result<Value, Error> {
+    fn eval_token(&mut self, token: &str) -> Result {
+        if let Ok(number) = token.parse::<i32>() {
+            self.stack.push(number)
+        } else {
+            let binary = "+ - * / SWAP OVER";
+            match token {
+                s if self.words.keys().any(|word| word == s) => {
+                    let token_stream = self.words[s].split_whitespace();
+                    for token in token_stream {
+                        self.token_stream.push(token.to_string());
+                    }
+                }
+
+                s if binary.split_whitespace().any(|operator| s == operator) => {
+                    self.binary_operation(&token)?
+                }
+                "DUP" => self.unary_operation(&token)?,
+                "DROP" => self.unary_operation(&token)?,
+                ":" => self.define_word()?,
+                _ => return Err(Error::UnknownWord),
+            }
+        }
+        Ok(())
+    }
+
+    fn pop_value(&mut self) -> std::result::Result<Value, Error> {
         if let Some(operand) = self.stack.pop() {
             Ok(operand)
         } else {
@@ -56,7 +78,7 @@ impl Forth {
     }
 
     fn binary_operation(&mut self, operator: &str) -> Result {
-        let (op1, op2) = (self.pop()?, self.pop()?);
+        let (op1, op2) = (self.pop_value()?, self.pop_value()?);
         match operator {
             "+" => self.stack.push(op2 + op1),
             "-" => self.stack.push(op2 - op1),
@@ -82,7 +104,7 @@ impl Forth {
     }
 
     fn unary_operation(&mut self, operator: &str) -> Result {
-        let op = self.pop()?;
+        let op = self.pop_value()?;
         match operator {
             "DUP" => {
                 self.stack.push(op);
@@ -91,6 +113,34 @@ impl Forth {
             "DROP" => {}
             _ => return Err(Error::UnknownWord),
         }
+        Ok(())
+    }
+
+    fn define_word(&mut self) -> Result {
+        let word = self.token_stream.pop().ok_or(Error::InvalidWord)?;
+        if let Ok(_number) = word.parse::<Value>() {
+            return Err(Error::InvalidWord);
+        };
+        let mut word_sequence: Vec<String> = vec![];
+        loop {
+            let current_token = self.token_stream.pop().ok_or(Error::InvalidWord)?;
+            match current_token.as_str() {
+                ";" => break,
+                s if self.words.keys().any(|word| word == &s) => self.words[s]
+                    .split_whitespace()
+                    .map(|token| token.to_string())
+                    .for_each(|token| word_sequence.push(token)),
+                _ => word_sequence.push(current_token.to_string()),
+            }
+        }
+        let word_sequence: String = word_sequence
+            .into_iter()
+            .map(|word| word.to_ascii_uppercase())
+            .rev()
+            .collect::<Vec<String>>()
+            .join(" ");
+        println!("Inserting sequence {} for word {}!", word_sequence, word);
+        self.words.insert(word, word_sequence);
         Ok(())
     }
 }
--- forth/tests/forth.rs
+++ forth/tests/forth.rs
@@ -237,7 +237,7 @@ fn over_error() {
 // User-defined words
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_consist_of_built_in_words() {
     let mut f = Forth::new();
     assert!(f.eval(": dup-twice dup dup ;").is_ok());
@@ -246,7 +246,7 @@ fn can_consist_of_built_in_words() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn execute_in_the_right_order() {
     let mut f = Forth::new();
     assert!(f.eval(": countup 1 2 3 ;").is_ok());
@@ -255,7 +255,7 @@ fn execute_in_the_right_order() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn redefining_an_existing_word() {
     let mut f = Forth::new();
     assert!(f.eval(": foo dup ;").is_ok());
@@ -265,7 +265,7 @@ fn redefining_an_existing_word() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn redefining_an_existing_built_in_word() {
     let mut f = Forth::new();
     assert!(f.eval(": swap dup ;").is_ok());
@@ -274,7 +274,7 @@ fn redefining_an_existing_built_in_word() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn user_defined_words_are_case_insensitive() {
     let mut f = Forth::new();
     assert!(f.eval(": foo dup ;").is_ok());
@@ -283,7 +283,7 @@ fn user_defined_words_are_case_insensitive() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn definitions_are_case_insensitive() {
     let mut f = Forth::new();
     assert!(f.eval(": SWAP DUP Dup dup ;").is_ok());
@@ -292,7 +292,7 @@ fn definitions_are_case_insensitive() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn redefining_a_built_in_operator() {
     let mut f = Forth::new();
     assert!(f.eval(": + * ;").is_ok());
@@ -301,7 +301,7 @@ fn redefining_a_built_in_operator() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_use_different_words_with_the_same_name() {
     let mut f = Forth::new();
     assert!(f.eval(": foo 5 ;").is_ok());
@@ -312,7 +312,7 @@ fn can_use_different_words_with_the_same_name() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_define_word_that_uses_word_with_the_same_name() {
     let mut f = Forth::new();
     assert!(f.eval(": foo 10 ;").is_ok());
@@ -322,14 +322,14 @@ fn can_define_word_that_uses_word_with_the_same_name() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn defining_a_number() {
     let mut f = Forth::new();
     assert_eq!(Err(Error::InvalidWord), f.eval(": 1 2 ;"));
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn malformed_word_definition() {
     let mut f = Forth::new();
     assert_eq!(Err(Error::InvalidWord), f.eval(":"));
@@ -338,14 +338,14 @@ fn malformed_word_definition() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn calling_non_existing_word() {
     let mut f = Forth::new();
     assert_eq!(Err(Error::UnknownWord), f.eval("1 foo"));
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn multiple_definitions() {
     let mut f = Forth::new();
     assert!(f.eval(": one 1 ; : two 2 ; one two +").is_ok());
@@ -353,7 +353,7 @@ fn multiple_definitions() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn definitions_after_ops() {
     let mut f = Forth::new();
     assert!(f.eval("1 2 + : addone 1 + ; addone").is_ok());
@@ -361,7 +361,7 @@ fn definitions_after_ops() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn redefine_an_existing_word_with_another_existing_word() {
     let mut f = Forth::new();
     assert!(f.eval(": foo 5 ;").is_ok());
