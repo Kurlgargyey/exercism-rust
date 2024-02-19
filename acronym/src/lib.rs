b61c0a495b91d53eb6c21acf2d85377049a52b23
RK <nahsirkivar@gmail.com>
2024-02-19 10:55:18 +0100

    unify binding names

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -4,19 +4,19 @@ pub fn abbreviate(phrase: &str) -> String {
         .filter(|word| !word.is_empty())
         .map(|word| {
             word.chars()
-                .filter(|ch| !ch.is_ascii_punctuation())
+                .filter(|c| !c.is_ascii_punctuation())
                 .collect::<String>()
         })
         .map(|word| {
-            if word.chars().all(|ch| ch.is_ascii_uppercase()) {
+            if word.chars().all(|c| c.is_ascii_uppercase()) {
                 word.chars().next().unwrap().to_string()
             } else {
                 word.chars()
                     .enumerate()
-                    .filter(|(pos, char)| {
-                        char.is_alphanumeric() && (*pos == 0usize || char.is_ascii_uppercase())
+                    .filter(|(pos, c)| {
+                        c.is_alphanumeric() && (*pos == 0usize || c.is_ascii_uppercase())
                     })
-                    .map(|(_, char)| char.to_ascii_uppercase())
+                    .map(|(_, c)| c.to_ascii_uppercase())
                     .collect::<String>()
             }
         })
