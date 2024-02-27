1be22415c71dec563af5cf6993c21e894d63b17c
RK <nahsirkivar@gmail.com>
2024-02-27 17:25:56 +0100

    wip: common crates only

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -105,3 +105,7 @@ fn generate_possible_maps(
     }
     possible_combinations
 }
+
+fn generate_combinations(char_count: usize) -> Vec<Vec<i32>> {
+    let digits = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+}
