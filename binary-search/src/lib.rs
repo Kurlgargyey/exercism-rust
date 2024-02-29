a6456b90c5b2542274ce7d062b815962f03f6305
Ravi Krishan <ravi.krishan@ccontor.de>
2024-02-29 17:15:03 +0100

    trying to use closures

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,5 +1,28 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+    if array.len() == 0 {
+        return None;
+    }
+    let mut lower_bound = 0;
+    let mut upper_bound = array.len();
+    let get_idx = || (upper_bound - lower_bound) / 2 - 1;
+    let mut idx = get_idx();
+    loop {
+        let middle_value = array[idx];
+        match middle_value {
+            x if x > key => {
+                upper_bound = idx + 1;
+                idx = get_idx();
+            }
+            x if x < key => {
+                lower_bound = idx + 1;
+                idx = get_idx();
+            }
+            _ => {
+                return Some(idx);
+            }
+        }
+        if lower_bound > upper_bound {
+            return None;
+        }
+    }
 }
