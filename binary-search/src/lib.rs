a067d3aeb5493147dc98e6a2b4ed8633352d748a
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-04 12:22:56 +0100

    passing solution

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -2,27 +2,22 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
     if array.len() == 0 {
         return None;
     }
-    let mut lower_bound = 0;
-    let mut upper_bound = array.len();
-    let get_idx = || (upper_bound - lower_bound) / 2 - 1;
-    let mut idx = get_idx();
-    loop {
-        let middle_value = array[idx];
+    let mut lower_bound: isize = 0;
+    let mut upper_bound: isize = (array.len() - 1) as isize;
+    while lower_bound <= upper_bound {
+        let idx = lower_bound + (upper_bound - lower_bound) / 2;
+        let middle_value = array[idx as usize];
         match middle_value {
             x if x > key => {
-                upper_bound = idx + 1;
-                idx = get_idx();
+                upper_bound = idx - 1;
             }
             x if x < key => {
                 lower_bound = idx + 1;
-                idx = get_idx();
             }
             _ => {
-                return Some(idx);
+                return Some(idx as usize);
             }
         }
-        if lower_bound > upper_bound {
-            return None;
-        }
     }
+    None
 }
