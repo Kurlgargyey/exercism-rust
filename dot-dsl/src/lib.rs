d36aa448f9c033e137cca4cb0d53f16f3ce49397
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-04 17:03:47 +0100

    starting to build the data structure

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,9 +1,33 @@
 pub mod graph {
-    pub struct Graph;
+    use std::collections::HashMap;
+    use crate::graph::graph_items::*;
+    pub struct Graph {
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: HashMap<String, String>,
+    }
+
+    pub mod graph_items {
+        pub struct Edge;
+        pub struct Node;
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            todo!("Construct a new Graph struct.");
+            Graph {
+                nodes: Vec::<Node>::new(),
+                edges: Vec::<Edge>::new(),
+                attrs: HashMap::<String, String>::new(),
+            }
+        }
+        pub fn with_nodes(&self, nodes: &[&str]) -> Self {
+            todo!("with nodes")
+        }
+        pub fn with_attrs(&self, attrs: &Vec<&str>) -> Self {
+            todo!("with attrs")
+        }
+        pub fn with_edges(&self, edges: &Vec<(&str, &str)>) -> Self {
+            todo!("with edges")
         }
     }
 }
