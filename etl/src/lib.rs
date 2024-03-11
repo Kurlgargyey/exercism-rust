f52f0f9c72ed2aab32faba02c08bd474c010326c
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-11 16:24:46 +0100

    passing solution

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,5 +1,10 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    todo!("How will you transform the tree {h:?}?")
+    h.iter().fold(BTreeMap::<char, i32>::new(), |mut acc, (value, letters)| {
+        for letter in letters {
+            acc.entry(letter.to_ascii_lowercase()).or_insert(*value);
+        }
+        acc
+    })
 }
