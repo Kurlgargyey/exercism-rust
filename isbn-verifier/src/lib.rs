8855bbac7a8d7692a77857fb8e35a056527ec758
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-13 12:26:54 +0100

    passing implementation

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,30 +1,32 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let mut result = 0;
-    let digit_iter = isbn
-        .chars()
-        .filter(|c| *c != '-')
-        .enumerate();
+    let mut checksum = 0;
+    let mut digit_iter = isbn.chars().filter(|c| *c != '-');
 
-    for (idx, char) in digit_iter {
-        if idx > 9 {
-            return false;
-        }
-        match char {
-            c if c.is_digit(10) => {
-                result += (10 - idx) * (char.to_digit(10).unwrap() as usize);
-            }
-            'X' => {
-                if idx != 9 {
+    for i in 0..10 {
+        if let Some(char) = digit_iter.next() {
+            match char {
+                c if c.is_digit(10) => {
+                    checksum += (10 - i) * (char.to_digit(10).unwrap() as usize);
+                }
+                'X' => {
+                    if i != 9 {
+                        return false;
+                    }
+                    checksum += 10;
+                }
+                _ => {
                     return false;
                 }
-                result += (10 - idx) * 10;
             }
-            _ => {
-                return false;
-            }
+        } else {
+            return false;
         }
     }
 
-    result % 11 == 0
+    if let Some(_) = digit_iter.next() {
+        return false;
+    }
+
+    checksum % 11 == 0
 }
