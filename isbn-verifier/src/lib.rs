078b5dc3d1be4620d4ffebb0058c4212e2073cde
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-13 08:39:18 +0100

    first attempt at isbn-verifier

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,30 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    todo!("Is {isbn:?} a valid ISBN number?");
+    let mut result = 0;
+    let digit_iter = isbn
+        .chars()
+        .filter(|c| *c != '-')
+        .enumerate();
+
+    for (idx, char) in digit_iter {
+        if idx > 9 {
+            return false;
+        }
+        match char {
+            c if c.is_digit(10) => {
+                result += (10 - idx) * (char.to_digit(10).unwrap() as usize);
+            }
+            'X' => {
+                if idx != 9 {
+                    return false;
+                }
+                result += (10 - idx) * 10;
+            }
+            _ => {
+                return false;
+            }
+        }
+    }
+
+    result % 11 == 0
 }
