b27a7f2dd1c5108aa9356b61909138ec25b9b33a
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-14 14:07:08 +0100

    rewrite unwieldy match statement

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -2,14 +2,12 @@ use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
     let mut letters = HashSet::<char>::new();
-    for char in candidate.to_ascii_lowercase().chars() {
-        match char {
-            c if !c.is_alphabetic() => {
-                continue;
-            }
-            _ => if !letters.insert(char) {
-                return false;
-            }
+    for char in candidate
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .map(|c| c.to_ascii_lowercase()) {
+        if !letters.insert(char) {
+            return false;
         }
     }
     true
