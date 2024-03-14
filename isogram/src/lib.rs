af804ba41a52ef4408f87224139cc1cd30ced80d
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-14 14:12:20 +0100

    lowercasing the str beforehand is faster for unicode-compatible implementation

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -3,8 +3,8 @@ use std::collections::HashSet;
 pub fn check(candidate: &str) -> bool {
     let mut letters = HashSet::new();
     candidate
+        .to_lowercase()
         .chars()
         .filter(|c| c.is_alphabetic())
-        .map(|c| c.to_lowercase().to_string())
         .all(|c| letters.insert(c))
 }
