b0e673bd90aa1d836c5b43019d1a36868ec2bdba
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-20 22:37:52 +0100

    working lazy iter implementation?

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -32,12 +32,45 @@ impl PalindromeCheck for String {
     }
 }
 
+trait ProductCheck {
+    fn is_product_of(&self, min: u64, max: u64) -> bool;
+}
+
+impl ProductCheck for u64 {
+    fn is_product_of(&self, min: u64, max: u64) -> bool {
+        (min..=max).any(|i| { self % i == 0 && (min..=max).contains(&(self / i)) })
+    }
+}
+
+trait PalindromeProductCheck {
+    fn is_palindrome_product_of(&self, min: &u64, max: &u64) -> bool;
+}
+
+impl PalindromeProductCheck for u64 {
+    fn is_palindrome_product_of(&self, min: &u64, max: &u64) -> bool {
+        self.to_string().is_palindrome() && self.is_product_of(*min, *max)
+    }
+}
+
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
+    /*
     let mut products = generate_products(min, max);
     products.sort();
     println!("sorted vec of products: {:?}", products);
-    let min_pal = first_palindrome(&products)?;
-    let max_pal = first_palindrome(&products.into_iter().rev().collect())?;
+    */
+
+    let mut range = (min.pow(2)..=max.pow(2)).skip_while(
+        |candidate| !candidate.is_palindrome_product_of(&min, &max)
+    );
+    let mut rev_range = range
+        .clone()
+        .collect::<Vec<u64>>()
+        .into_iter()
+        .rev()
+        .skip_while(|candidate| !candidate.is_palindrome_product_of(&min, &max));
+
+    let min_pal = Palindrome::new(range.next()?)?;
+    let max_pal = Palindrome::new(rev_range.next()?)?;
     Some((min_pal, max_pal))
 }
 
