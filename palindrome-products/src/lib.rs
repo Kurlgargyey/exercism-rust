7dacd139b8a3bddc0758e8c23117601f3287a6df
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-20 22:52:41 +0100

    clearer naming and syntax in palindrome_products()

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -44,24 +44,25 @@ impl ProductCheck for u64 {
 
 trait PalindromeProductCheck {
     fn is_palindrome_product_of(&self, min: &u64, max: &u64) -> bool;
+    fn new_palindrome_product(&self, min: &u64, max: &u64) -> Option<Palindrome>;
 }
 
 impl PalindromeProductCheck for u64 {
     fn is_palindrome_product_of(&self, min: &u64, max: &u64) -> bool {
         self.to_string().is_palindrome() && self.is_product_of(*min, *max)
     }
+
+    fn new_palindrome_product(&self, min: &u64, max: &u64) -> Option<Palindrome> {
+        if self.is_product_of(*min, *max) { Palindrome::new(*self) } else { None }
+    }
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    /*
-    let mut products = generate_products(min, max);
-    products.sort();
-    println!("sorted vec of products: {:?}", products);
-    */
+    let mut range = min.pow(2)..=max.pow(2);
+    let smallest_palindrome = range.find_map(|candidate|
+        candidate.new_palindrome_product(&min, &max)
+    )?;
 
-    let mut range = (min.pow(2)..=max.pow(2)).skip_while(
-        |candidate| !candidate.is_palindrome_product_of(&min, &max)
-    );
     let mut rev_range = range
         .clone()
         .collect::<Vec<u64>>()
@@ -69,59 +70,6 @@ pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome
         .rev()
         .skip_while(|candidate| !candidate.is_palindrome_product_of(&min, &max));
 
-    let min_pal = Palindrome::new(range.next()?)?;
-    let max_pal = Palindrome::new(rev_range.next()?)?;
-    Some((min_pal, max_pal))
-}
-
-fn generate_products(min: u64, max: u64) -> Vec<u64> {
-    let mut products = Vec::<u64>::new();
-    let mut range: Vec<u64> = (min.pow(2)..=max.pow(2)).collect();
-
-    for i in min..=max {
-        range.retain(|number| {
-            if number % i == 0 && (min..=max).contains(&(number / i)) {
-                products.push(*number);
-                return false;
-            }
-            true
-        });
-    }
-
-    products.into_iter().collect()
-    /*
-    (min..=max)
-        .fold(products, |mut set, number| {
-            for number2 in min..=max {
-                set.insert(number * number2);
-            }
-            set
-        })
-        .into_iter()
-        .collect()
-    */
-    /*
-    (0..=max.pow(2))
-    .filter(|number| {
-        (min..=max).any(|factor1|
-                (min..=max).any(|factor2| {
-                    let test = number % factor1 == 0 && number / factor1 == factor2;
-                    if test {
-                        println!("{:?} is the product of {:?} and {:?}!", number, factor1, factor2);
-                    }
-                    test
-                })
-            )
-        })
-        .collect()
-    */
-}
-
-fn first_palindrome(numbers: &Vec<u64>) -> Option<Palindrome> {
-    for i in numbers {
-        if let Some(palindrome) = Palindrome::new(*i) {
-            return Some(palindrome);
-        }
-    }
-    None
+    let largest_palindrome = Palindrome::new(rev_range.next()?)?;
+    Some((smallest_palindrome, largest_palindrome))
 }
