84f108d79a3654e904a03a3fa670f87542ed8321
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-20 23:27:19 +0100

    new_palindrome_product() was slow and pointless

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -57,32 +57,24 @@ impl ProductCheck for u64 {
 
 trait PalindromeProductCheck {
     fn is_palindrome_product_of(&self, min: &u64, max: &u64) -> bool;
-    fn new_palindrome_product(&self, min: &u64, max: &u64) -> Option<Palindrome>;
 }
 
 impl PalindromeProductCheck for u64 {
     fn is_palindrome_product_of(&self, min: &u64, max: &u64) -> bool {
         self.to_string().is_palindrome() && self.is_product_of(*min, *max)
     }
-
-    fn new_palindrome_product(&self, min: &u64, max: &u64) -> Option<Palindrome> {
-        if self.is_product_of(*min, *max) { Palindrome::new(*self) } else { None }
-    }
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
     let range = min.pow(2)..=max.pow(2);
-    let smallest_palindrome = range
-        .clone()
-        .find_map(|candidate| candidate.new_palindrome_product(&min, &max))?;
+    let smallest_palindrome = Palindrome::new(
+        range.clone().find(|candidate| candidate.is_palindrome_product_of(&min, &max))?
+    )?;
 
-    let mut rev_range = range
-        .clone()
-        .collect::<Vec<u64>>()
-        .into_iter()
-        .rev()
-        .skip_while(|candidate| !candidate.is_palindrome_product_of(&min, &max));
+    let mut rev_range = range.clone().rev();
+    let largest_palindrome = Palindrome::new(
+        rev_range.find(|candidate| candidate.is_palindrome_product_of(&min, &max))?
+    )?;
 
-    let largest_palindrome = Palindrome::new(rev_range.next()?)?;
     Some((smallest_palindrome, largest_palindrome))
 }
