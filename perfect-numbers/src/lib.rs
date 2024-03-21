762abd12a95587f79d6b68ad076d5e1291351e29
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-21 16:24:57 +0100

    working solution

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,5 +6,17 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    todo!("classify {num}");
+    if num == 0 {
+        return None;
+    }
+    match aliquot(num) {
+        i if i == num => Some(Classification::Perfect),
+        i if i > num => Some(Classification::Abundant),
+        i if i < num => Some(Classification::Deficient),
+        _ => None,
+    }
+}
+
+fn aliquot(num: u64) -> u64 {
+    (1..num).fold(0, |acc, number| if num % number == 0 { acc + number } else { acc })
 }
