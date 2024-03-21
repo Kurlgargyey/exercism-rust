b1923f98508419a009adad8ad01fce6a3e597dc2
Ravi Krishan <ravi.krishan@ccontor.de>
2024-03-21 09:19:02 +0100

    more concise

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,6 +1,4 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let mut letters = 'a'..='z';
-
-    letters.all(|char| sentence.to_lowercase().contains(char))
+    ('a'..='z').all(|char| sentence.to_lowercase().contains(char))
 }
