303f341ffdcadc8eb105250a1f5993ef9ff02cb0
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-21 20:20:45 +0100

    working solution

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,3 +1,41 @@
+use itertools::Itertools;
+
 pub fn translate(input: &str) -> String {
-    todo!("Using the Pig Latin text transformation rules, convert the given input '{input}'");
+    input
+        .split_ascii_whitespace()
+        .map(|word| translate_word(word))
+        .join(" ")
+}
+
+fn parse_beginning(input: &str) -> String {
+    let mut input_iter = input.chars();
+    let vowels = "aeiou";
+    let mut beginning = "".to_string();
+
+    for char in &mut input_iter {
+        match char {
+            c if c == 'u' && beginning.ends_with("q") => {
+                beginning.push(char);
+                return beginning;
+            }
+            _ if beginning == "yt" || beginning == "xr" => {
+                return "".to_string();
+            }
+            c if (c == 'y' && !beginning.is_empty()) || vowels.contains(c) => {
+                return beginning;
+            }
+            _ => {
+                beginning.push(char);
+            }
+        }
+    }
+    beginning
+}
+
+fn translate_word(input: &str) -> String {
+    let beginning = parse_beginning(input);
+    let mut result = input.chars().skip(beginning.len()).collect::<String>();
+    result.push_str(beginning.as_str());
+    result.push_str("ay");
+    result
 }
