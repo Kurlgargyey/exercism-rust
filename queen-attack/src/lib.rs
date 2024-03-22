caa6b4fddbd6dc4d1165332ddb0c9dfdd965d48d
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-22 11:16:01 +0100

    first couple tests passing

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,23 +1,61 @@
-#[derive(Debug)]
-pub struct ChessPosition;
+use std::iter::successors;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+    diagonals: Vec<ChessPosition>,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        todo!(
-            "Construct a ChessPosition struct, given the following rank, file: ({rank}, {file}). If the position is invalid return None."
-        );
+        let valid_range = 0..8;
+        (valid_range.contains(&rank) && valid_range.contains(&file)).then_some(ChessPosition {
+            rank,
+            file,
+        })
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        todo!("Given the chess position {position:?}, construct a Queen struct.");
+        let mut diagonals = Vec::<ChessPosition>::new();
+        for position in successors(Some(position.clone()), |pos| {
+            ChessPosition::new(pos.rank - 1, pos.file + 1)
+        }) {
+            diagonals.push(position);
+        }
+        for position in successors(Some(position.clone()), |pos| {
+            ChessPosition::new(pos.rank + 1, pos.file + 1)
+        }) {
+            diagonals.push(position);
+        }
+        for position in successors(Some(position.clone()), |pos| {
+            ChessPosition::new(pos.rank + 1, pos.file - 1)
+        }) {
+            diagonals.push(position);
+        }
+        for position in successors(Some(position.clone()), |pos| {
+            ChessPosition::new(pos.rank - 1, pos.file - 1)
+        }) {
+            diagonals.push(position);
+        }
+        Queen { position, diagonals }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        todo!("Determine if this Queen can attack the other Queen {other:?}");
+        if self.position.rank == other.position.rank {
+            return true;
+        } else if self.position.file == other.position.file {
+            return true;
+        } else if self.diagonals.contains(&other.position) {
+            return true;
+        }
+        false
     }
 }
--- queen-attack/tests/queen-attack.rs
+++ queen-attack/tests/queen-attack.rs
@@ -7,35 +7,35 @@ fn queen_with_a_valid_position() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn queen_must_have_positive_row() {
     let chess_position = ChessPosition::new(-2, 2);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn queen_must_have_row_on_board() {
     let chess_position = ChessPosition::new(8, 4);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn queen_must_have_positive_column() {
     let chess_position = ChessPosition::new(2, -2);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn queen_must_have_column_on_board() {
     let chess_position = ChessPosition::new(4, 8);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn cannot_attack() {
     let white_queen = Queen::new(ChessPosition::new(2, 4).unwrap());
     let black_queen = Queen::new(ChessPosition::new(6, 6).unwrap());
@@ -43,7 +43,7 @@ fn cannot_attack() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_attack_on_same_row() {
     let white_queen = Queen::new(ChessPosition::new(2, 4).unwrap());
     let black_queen = Queen::new(ChessPosition::new(2, 6).unwrap());
@@ -51,7 +51,7 @@ fn can_attack_on_same_row() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_attack_on_same_column() {
     let white_queen = Queen::new(ChessPosition::new(4, 5).unwrap());
     let black_queen = Queen::new(ChessPosition::new(2, 5).unwrap());
@@ -59,7 +59,7 @@ fn can_attack_on_same_column() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_attack_on_first_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(2, 2).unwrap());
     let black_queen = Queen::new(ChessPosition::new(0, 4).unwrap());
@@ -67,7 +67,7 @@ fn can_attack_on_first_diagonal() {
 }
 
 #[test]
-#[ignore]
+//#[ignore]
 fn can_attack_on_second_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(2, 2).unwrap());
     let black_queen = Queen::new(ChessPosition::new(3, 1).unwrap());
@@ -92,8 +92,7 @@ fn can_attack_on_fourth_diagonal() {
 
 #[test]
 #[ignore]
-fn cannot_attack_if_falling_diagonals_are_only_the_same_when_reflected_across_the_longest_falling_diagonal(
-) {
+fn cannot_attack_if_falling_diagonals_are_only_the_same_when_reflected_across_the_longest_falling_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(4, 1).unwrap());
     let black_queen = Queen::new(ChessPosition::new(2, 5).unwrap());
     assert!(!white_queen.can_attack(&black_queen));
