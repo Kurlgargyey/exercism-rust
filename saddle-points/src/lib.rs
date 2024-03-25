6b0e519c10a38f9159752a8fe5c4e6fd1e92a3aa
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-25 16:35:09 +0100

    attempt to fold into an option starting on None

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    todo!("find the saddle points of the following matrix: {input:?}")
+    let mut row_maxima = Vec::<((usize, usize), &u64)>::new();
+    for (row_idx, row) in input.iter().enumerate() {
+        let maximum = row
+            .iter()
+            .enumerate()
+            .fold(None, |acc, (col_idx, height)| {
+                if acc == None || *height > acc.1 {
+                    Some(((row_idx, col_idx), *height))
+                } else {
+                    acc
+                }
+            });
+    }
+    let mut results = Vec::<(usize, usize)>::new();
+    for ((row, col), height) in row_maxima {
+        if input.iter().all(|row| !row[col] > *height) {
+            results.push((row, col));
+        }
+    }
+    results
 }
