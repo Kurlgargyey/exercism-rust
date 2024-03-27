0e209afba59bf9db3cfd107c88cdcb9e1e93f815
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-27 16:32:48 +0100

    renamed 'candidate', because it is already a confirmed prime by the time of assignment

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -2,11 +2,11 @@ pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut candidates: Vec<u64> = (2..=upper_bound).collect();
     let mut primes: Vec<u64> = Vec::<u64>::new();
     loop {
-        if let Some(candidate) = candidates.clone().into_iter().next() {
-            primes.push(candidate);
+        if let Some(prime) = candidates.clone().into_iter().next() {
+            primes.push(prime);
             candidates = candidates
                 .into_iter()
-                .filter(|number| !(number % candidate == 0))
+                .filter(|number| !(number % prime == 0))
                 .collect();
         }
         if candidates.is_empty() {
