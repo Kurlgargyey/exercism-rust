d5c7764395f66dc19c0528160e2f3c7078072668
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-27 16:34:24 +0100

    better loop syntax

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,16 +1,13 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut candidates: Vec<u64> = (2..=upper_bound).collect();
     let mut primes: Vec<u64> = Vec::<u64>::new();
-    loop {
+    while !candidates.is_empty() {
         if let Some(prime) = candidates.clone().into_iter().next() {
             primes.push(prime);
             candidates = candidates
                 .into_iter()
                 .filter(|number| !(number % prime == 0))
                 .collect();
-        }
-        if candidates.is_empty() {
-            break;
         }
     }
     primes
