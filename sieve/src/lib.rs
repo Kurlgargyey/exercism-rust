6158d6e0ac9fd71fb4f4227d290a512d11f56bb2
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-27 16:31:58 +0100

    clearer names

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut candidate_vec: Vec<u64> = (2..=upper_bound).collect();
-    let mut result: Vec<u64> = Vec::<u64>::new();
+    let mut candidates: Vec<u64> = (2..=upper_bound).collect();
+    let mut primes: Vec<u64> = Vec::<u64>::new();
     loop {
-        if let Some(candidate) = candidate_vec.clone().into_iter().next() {
-            result.push(candidate);
-            candidate_vec = candidate_vec
+        if let Some(candidate) = candidates.clone().into_iter().next() {
+            primes.push(candidate);
+            candidates = candidates
                 .into_iter()
                 .filter(|number| !(number % candidate == 0))
                 .collect();
         }
-        if candidate_vec.is_empty() {
+        if candidates.is_empty() {
             break;
         }
     }
-    result
+    primes
 }
