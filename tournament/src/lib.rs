42589c06b3fa01bbc80f42d02604ca98a65c709f
Kurlgargyey <nahsirkivar@gmail.com>
2024-03-28 14:33:17 +0100

    beginning of exercise tournament

---
+++ tournament/src/lib.rs
@@ -0,0 +1,6 @@
+pub fn tally(match_results: &str) -> String {
+    let header = "Team                           | MP |  W |  D |  L |  P";
+    todo!(
+        "Given the result of the played matches '{match_results}' return a properly formatted tally table string."
+    );
+}
