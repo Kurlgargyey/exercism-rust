76066b4f5fdc69becf7a9c525dc167ae544d446b
Kurlgargyey <nahsirkivar@gmail.com>
2024-04-10 01:03:25 +0200

    breaking up the program into more meaningful functions

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -4,6 +4,14 @@ pub struct Triangle<T>([T; 3]);
 
 impl<T: Into<f64> + PartialEq + Clone + Copy> Triangle<T> {
     pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        if !Self::triangle_test(&sides) {
+            return None;
+        }
+
+        Some(Triangle(sides))
+    }
+
+    fn triangle_test(sides: &[T; 3]) -> bool {
         let a: f64 = sides[0].into();
         let b: f64 = sides[1].into();
         let c: f64 = sides[2].into();
@@ -15,17 +23,12 @@ impl<T: Into<f64> + PartialEq + Clone + Copy> Triangle<T> {
         let sum = (angle_a + angle_b + angle_c) as f32;
         println!("the three angles are {angle_a}, {angle_b}, {angle_c}");
         println!("the sum of all angles is {sum}");
-
-        if sum != std::f32::consts::PI {
-            return None;
-        }
-
-        Some(Triangle(sides))
+        sum != std::f32::consts::PI
     }
-
     fn angle(side1: f64, side2: f64, side3: f64) -> f64 {
-        ((-side3.powf(2.0) + side1.powf(2.0) + side2.powf(2.0)) / 2.0 / side1 / side2).acos()
+        ((side1.powf(2.0) + side2.powf(2.0) - side3.powf(2.0)) / 2.0 / side1 / side2).acos()
     }
+
     pub fn is_equilateral(&self) -> bool {
         let mut sides = self.0.into_iter();
         let first = sides.next().unwrap();
