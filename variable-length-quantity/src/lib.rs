58a77cf916848f8677fdc2d992cd3e51ec5f61e5
Kurlgargyey <nahsirkivar@gmail.com>
2024-04-12 12:30:32 +0200

    beginning of variable-length-quantity

---
+++ variable-length-quantity/src/lib.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    IncompleteNumber,
+}
+
+/// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
+pub fn to_bytes(values: &[u32]) -> Vec<u8> {
+    for value in values {
+        
+    }
+}
+
+/// Given a stream of bytes, extract all numbers which are encoded in there.
+pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
+    todo!("Convert the list of bytes {bytes:?} to a list of numbers")
+}
