fe0e8c061a53dde587ba6bcf3ede012de992e1ec
Kurlgargyey <nahsirkivar@gmail.com>
2024-04-12 13:45:33 +0200

    encoding 0 correctly

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -5,9 +5,25 @@ pub enum Error {
 
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
+    let mut result = Vec::<u8>::new();
     for value in values {
-        
+        result.append(&mut encode(*value));
     }
+    result
+}
+
+fn encode(number: u32) -> Vec<u8> {
+    let mut result = Vec::<u8>::new();
+    let mut number = number;
+    let preceding_byte: u8 = 0b10000000;
+    let final_byte: u8 = 0b01111111;
+    result.push(final_byte & number.to_ne_bytes().iter().next().unwrap());
+    number = number << 7;
+    while number != 0 {
+        result.push(preceding_byte & number.to_ne_bytes().iter().next().unwrap());
+        number = number << 7;
+    }
+    result
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
