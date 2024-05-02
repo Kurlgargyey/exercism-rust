2c3191c46e376e15103f09706da7c33751483527
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-02 12:53:54 +0200

    reject bad format

--- wordy/src/lib.rs
+++ wordy/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn answer(command: &str) -> Option<i32> {
-    todo!("Return the result of the command '{command}' or None, if the command is invalid.");
+    if !command.ends_with('?') {
+        return None;
+    }
+
+    let mut words = command.split_ascii_whitespace();
+
+    match (words.next(), words.next()) {
+        (Some("What"), Some("is")) => {}
+        _ => return None,
+    }
+
+    Some(1)
 }
