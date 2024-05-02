485420b2acb23f70d458a0f33e1c5eafc0b7c9ff
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-02 10:39:07 +0200

    callbacks run sometimes

--- react/src/lib.rs
+++ react/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
 /// `InputCellId` is a unique identifier for an input cell.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -34,9 +34,10 @@ struct ComputeCell<T> {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct CallbackId(u32);
 
+
 struct Callback<'c, T: Clone> {
     compute_cell: ComputeCellId,
-    function: Box<dyn FnMut(T) + 'c>,
+    function: Arc<Mutex<dyn FnMut(T) + 'c>>,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -177,21 +178,22 @@ impl<'r, 'e:'r, T: Copy + PartialEq> Reactor<'r, T> {
         }
     }
 
-    fn run_callbacks(&mut self, _id: InputCellId) {
-        /*
+    fn run_callbacks(&mut self, id: InputCellId) {
+
         for callback_id in &self.input_cells.get(&id).unwrap().callbacks {
             let compute_cell: ComputeCellId;
-            let mut function: Box<dyn FnMut(T)>;
+            let function: Arc<Mutex<dyn FnMut(T)>>;
+            let compute_value: T;
             {
                 let callback = self.callbacks.get_mut(&callback_id).unwrap();
                 compute_cell = callback.compute_cell;
-                function = callback.function;
+                function = callback.function.clone();
             }
-            let cell_value = self.value(CellId::Compute(compute_cell)).unwrap().clone();
-            (function)(cell_value);
+            compute_value = self.value(CellId::Compute(compute_cell)).unwrap().clone();
+            (function.lock().unwrap())(compute_value);
         }
-        */
-        todo!("implement way to run callbacks")
+
+
     }
 
     // Adds a callback to the specified compute cell.
@@ -216,7 +218,7 @@ impl<'r, 'e:'r, T: Copy + PartialEq> Reactor<'r, T> {
         }
         let callback = Callback {
             compute_cell: id,
-            function: Box::new(callback_function),
+            function: Arc::new(Mutex::new(callback_function)),
         };
         let callback_id = CallbackId(self.next_callback);
         self.next_callback += 1;
