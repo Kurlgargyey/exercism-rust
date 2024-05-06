7f1583f54e4952e82f9b0e5ebb47b9bcc6ede85b
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-06 14:33:46 +0200

    passing all tests

--- eliuds-eggs/src/lib.rs
+++ eliuds-eggs/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+    let mut result: usize = 0;
+    let mut display_value = display_value;
+    while display_value > 0 {
+        match display_value % 2 {
+            0 => (),
+            _ => result += 1,
+        }
+        display_value /= 2;
+    }
+    result
 }
