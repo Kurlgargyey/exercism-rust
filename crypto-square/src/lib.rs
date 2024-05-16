85788ecbf1c1bd3e91cf1ecc068c8cbebe8b43e7
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-16 09:58:04 +0200

    match instead of if guard

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -1,52 +1,53 @@
 pub fn encrypt(input: &str) -> String {
-    if input.is_empty() {
-        return String::new();
-    };
+    match input.is_empty() {
+        true => String::new(),
+        false => {
+            let mut sanitized_input: String = input
+                .chars()
+                .filter(|c| c.is_ascii_alphanumeric())
+                .map(|c| c.to_ascii_lowercase())
+                .collect();
 
-    let mut sanitized_input: String = input
-        .chars()
-        .filter(|c| c.is_ascii_alphanumeric())
-        .map(|c| c.to_ascii_lowercase())
-        .collect();
+            let (rows, cols) = find_rectangle(sanitized_input.len());
 
-    let (rows, cols) = find_rectangle(sanitized_input.len()).unwrap();
+            let difference = rows * cols - sanitized_input.len();
 
-    let difference = rows * cols - sanitized_input.len();
+            for _i in 1..=difference {
+                sanitized_input.push(' ');
+            }
 
-    for _i in 1..=difference {
-        sanitized_input.push(' ');
+            let col_vec: Vec<Vec<char>> = sanitized_input
+                .chars()
+                .collect::<Vec<char>>()
+                .chunks(rows)
+                .map(|chunk| chunk.to_vec())
+                .collect();
+
+            let mut result: Vec<String> = Vec::<String>::new();
+
+            for i in 0..rows {
+                let row: String = col_vec.iter().fold(String::new(), |mut result, col| {
+                    result.push(col[i]);
+                    result
+                });
+
+                result.push(row);
+            }
+
+            result.join(" ")
+        }
     }
-
-    let vec: Vec<Vec<char>> = sanitized_input
-        .chars()
-        .collect::<Vec<char>>()
-        .chunks(rows)
-        .map(|chunk| chunk.to_vec())
-        .collect();
-
-    let mut result: Vec<String> = Vec::<String>::new();
-
-    for i in 0..rows {
-        let row: String = vec.iter().fold(String::new(), |mut result, col| {
-            result.push(col[i]);
-            result
-        });
-
-        result.push(row);
-    }
-
-    result.join(" ")
 }
 
-fn find_rectangle(length: usize) -> Option<(usize, usize)> {
+fn find_rectangle(length: usize) -> (usize, usize) {
     let floor = (length as f64).sqrt().floor() as usize;
 
     for i in floor..=length {
         if are_valid_dimensions(i, floor, length) {
-            return Some((i, floor));
+            return (i, floor);
         }
     }
-    None
+    (0, 0)
 }
 
 fn are_valid_dimensions(side1: usize, side2: usize, length: usize) -> bool {
