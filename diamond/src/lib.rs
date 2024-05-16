4d40ec91d635d92dd3aa5bf2d677fbcefec07ba3
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-16 12:42:35 +0200

    first attempt: alloc empty string and replace letters

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,4 +1,15 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    let row_width = ('A'..=c).count()*2-1;
-    ('A'..=c).
+    let row_width = ('A'..=c).count() * 2 - 1;
+    let middle_idx = row_width / 2;
+    ('A'..=c)
+        .enumerate()
+        .fold(Vec::<String>::new(), |mut vec, (idx, char)| {
+            let mut row = String::with_capacity(row_width);
+            let first_idx = middle_idx - idx;
+            let second_idx = middle_idx + idx;
+            row.replace_range(first_idx..first_idx + 1, char.to_string().as_str());
+            row.replace_range(second_idx..second_idx + 1, char.to_string().as_str());
+            vec.push(row);
+            vec
+        })
 }
