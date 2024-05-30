491a2828d63c8a412f1731fa53d31144e8a8ad42
Kurlgargyey <nahsirkivar@gmail.com>
2024-05-30 23:37:25 +0200

    passing luhn-trait

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -7,8 +7,44 @@ pub trait Luhn {
 /// by hand for the every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl<'a> Luhn for &'a str {
+impl<T: ToString> Luhn for T {
     fn valid_luhn(&self) -> bool {
-        todo!("Determine if '{self}' is a valid credit card number.");
+        let cleaned_code = self
+            .to_string()
+            .trim()
+            .split_whitespace()
+            .collect::<String>();
+        if cleaned_code.len() <= 1 {
+            return false;
+        }
+        fn turn_into_digits(code: &String) -> Option<Vec<u32>> {
+            let mut output = Vec::<u32>::with_capacity(code.len());
+            for ch in code.chars().rev() {
+                output.push(ch.to_digit(10)?)
+            }
+            Some(output)
+        }
+        let digits = turn_into_digits(&cleaned_code);
+
+        match digits {
+            Some(vec) => {
+                let mut tmp: u32 = 0;
+                vec.into_iter()
+                    .enumerate()
+                    .fold(0, |chk, (idx, dgt)| match (idx + 1) % 2 {
+                        0 => {
+                            tmp = dgt * 2;
+                            if tmp > 9 {
+                                tmp -= 9
+                            }
+                            chk + tmp
+                        }
+                        _ => chk + dgt,
+                    })
+                    % 10
+                    == 0
+            }
+            None => false,
+        }
     }
 }
