b60b2e8e4497c015fdb402eb1ca8f88194ded9bd
Kurlgargyey <nahsirkivar@gmail.com>
2024-06-03 16:09:37 +0200

    final version rotational-cipher

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -2,16 +2,20 @@ pub fn rotate(input: &str, key: u8) -> String {
     input
         .chars()
         .map(|c| match c.is_alphabetic() {
-            true => map_char(c, key),
+            true => cipher_char(c, key),
             false => c,
         })
         .collect()
 }
 
-fn map_char(c: char, key: u8) -> char {
+fn cipher_char(c: char, key: u8) -> char {
     match c.is_lowercase() {
-        true => char::from_u32(((c as u32 - 'a' as u32 + key as u32) % 26) + 'a' as u32),
-        false => char::from_u32(((c as u32 - 'A' as u32 + key as u32) % 26) + 'A' as u32),
+        true => rotate_char(c, key, 'a'),
+        false => rotate_char(c, key, 'A'),
     }
-    .expect("invalid char!")
+}
+
+fn rotate_char(c: char, key: u8, offset: char) -> char {
+    char::from_u32(((c as u32 - offset as u32 + key as u32) % 26) + offset as u32)
+        .expect("invalid char!")
 }
