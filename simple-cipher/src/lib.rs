57b6f7122ac519c3d3d60056514e17839217aac0
Kurlgargyey <nahsirkivar@gmail.com>
2024-06-04 15:36:41 +0200

    passing simple-cipher

--- simple-cipher/src/lib.rs
+++ simple-cipher/src/lib.rs
@@ -1,11 +1,65 @@
+use rand::Rng;
+
 pub fn encode(key: &str, s: &str) -> Option<String> {
-    todo!("Use {key} to encode {s} using shift cipher")
+    if key.is_empty() {
+        return None;
+    }
+    let mut shiftgen = key.chars().map(|c| (c as i64 - 'a' as i64) as i8).cycle();
+
+    shift_string(&mut shiftgen, s)
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
-    todo!("Use {key} to decode {s} using shift cipher")
+    if key.is_empty() {
+        return None;
+    }
+    let mut shiftgen = key
+        .chars()
+        .map(|c| -((c as i64 - 'a' as i64) as i8))
+        .cycle();
+
+    shift_string(&mut shiftgen, s)
 }
 
 pub fn encode_random(s: &str) -> (String, String) {
-    todo!("Generate random key with only a-z chars and encode {s}. Return tuple (key, encoded s)")
+    let key = generate_key();
+    let cipher = encode(&key, s).unwrap();
+    (key, cipher)
+}
+
+fn generate_key() -> String {
+    let mut rng = rand::thread_rng();
+    let range = rand::distributions::Uniform::new('a', 'z');
+    (0..100).map(|_| rng.sample(range)).collect()
+}
+
+fn shift_string(shiftgen: &mut impl Iterator<Item = i8>, s: &str) -> Option<String> {
+    let mut result = String::new();
+    for char in s.chars() {
+        let key = shiftgen.next().expect("key ended for some reason");
+        if !(key >= -26) || !(key <= 26) {
+            return None;
+        }
+        result.push(cipher_char(char, key))
+    }
+    Some(result)
+}
+
+fn cipher_char(c: char, key: i8) -> char {
+    println!("char: {}, key: {}", c, key);
+    match c.is_lowercase() {
+        true => rotate_char(c, key, 'a'),
+        false => rotate_char(c, key, 'A'),
+    }
+}
+
+fn rotate_char(c: char, key: i8, offset: char) -> char {
+    let mut char_cycle = ('a'..='z').cycle();
+    let mut new_char_idx = c as i8 - offset as i8 + key;
+    if new_char_idx.is_negative() {
+        new_char_idx = 26 + new_char_idx
+    }
+    char_cycle
+        .nth(new_char_idx as usize)
+        .expect("something went wrong")
 }
