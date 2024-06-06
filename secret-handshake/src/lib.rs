86e95e120ca17baece456bcdda00f8a08b77de1c
Kurlgargyey <nahsirkivar@gmail.com>
2024-06-06 17:12:55 +0200

    passing secret-handshake

--- secret-handshake/src/lib.rs
+++ secret-handshake/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn actions(n: u8) -> Vec<&'static str> {
-    todo!("What is the secret handshake for {n}?")
+    let mut result = vec![];
+    if n & 0b00001 > 0 {
+        result.push("wink")
+    }
+    if n & 0b00010 > 0 {
+        result.push("double blink")
+    }
+    if n & 0b00100 > 0 {
+        result.push("close your eyes")
+    }
+    if n & 0b01000 > 0 {
+        result.push("jump")
+    }
+    if n & 0b10000 > 0 {
+        result.into_iter().rev().collect()
+    } else {
+        result
+    }
 }
